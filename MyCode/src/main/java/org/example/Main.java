e182501dd65abcc92dd94ca306cdd8b79fc4a03b
io23632 <io23632@bristol.ac.uk>
2024-02-01 14:30:43 +0000

    Added example Constructors Human

---
+++ MyCode/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Human patient1 = new Human("Jo", 28, 75);
+        Human patien2  = new Human("Sarah", 67, 67);
+
+        patien2.food();
+        patient1.drink();
+
+
+    }
+}

872b538e950a8d75abb35ef9cefb0cc44184355f
io23632 <io23632@bristol.ac.uk>
2024-02-01 14:11:33 +0000

    DEleted MyCOde

--- MyCode/JAVA_BroCode/BasicSyntax/src/main/java/org/Code/Main.java
+++
@@ -1,8 +0,0 @@
-package MyCode.JAVA_BroCode.BasicSyntax.src.main.java.org.Code;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bro Code Hello World");
-    }
-
-}

1a78d0eb8bb45e380d65ec193adc4f50b1b40f8a
io23632 <io23632@bristol.ac.uk>
2024-02-03 13:45:15 +0000

    [main]: added Inheritance Example From Bro Code

---
+++ Inheritance/src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // inheritance is the process where one class acquired the
+        // attributes and methods of another
+
+        }
+    }
